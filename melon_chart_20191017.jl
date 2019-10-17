719235e4f0a66f2f9df437e917bc6d145ab57c43
tada <imjunghee@imjeonghuiui-MacBook-Pro.local>
2019-10-17 21:18:52 +0900

    How to save Crawling Data

---
+++ melon_chart_20191017.jl
@@ -0,0 +1,100 @@
+{"title": "어떻게 이별까지 사랑하겠어, 널 사랑하는 거지", "rank": "1", "singer": "AKMU (악동뮤지션)", "album": "항해"}
+{"title": "새 사랑", "rank": "2", "singer": "송하예", "album": "새 사랑"}
+{"title": "흔들리는 꽃들 속에서 네 샴푸향이 느껴진거야", "rank": "3", "singer": "장범준", "album": "멜로가 체질 OST Part 3"}
+{"title": "떨어지는 낙엽까지도", "rank": "4", "singer": "헤이즈 (Heize)", "album": "만추"}
+{"title": "있어줘요", "rank": "5", "singer": "장덕철", "album": "있어줘요"}
+{"title": "조금 취했어 (Prod. 2soo)", "rank": "6", "singer": "임재현", "album": "조금 취했어"}
+{"title": "허전해", "rank": "7", "singer": "폴킴", "album": "정규 2집 '마음, 하나'"}
+{"title": "사랑이란 멜로는 없어", "rank": "8", "singer": "전상근", "album": "사랑이란 멜로는 없어"}
+{"title": "워커홀릭", "rank": "9", "singer": "볼빨간사춘기", "album": "Two Five"}
+{"title": "안녕", "rank": "10", "singer": "폴킴", "album": "호텔 델루나 OST Part.10"}
+{"title": "오늘도 빛나는 너에게 (To You My Light) (Feat.이라온)", "rank": "11", "singer": "마크툽 (MAKTUB)", "album": "Red Moon : To You My Light"}
+{"title": "시든 꽃에 물을 주듯", "rank": "12", "singer": "HYNN(박혜원)", "album": "시든 꽃에 물을 주듯"}
+{"title": "가을밤 떠난 너", "rank": "13", "singer": "케이시 (Kassy)", "album": "Rewind"}
+{"title": "Feel Special", "rank": "14", "singer": "TWICE (트와이스)", "album": "Feel Special"}
+{"title": "니 소식", "rank": "15", "singer": "송하예", "album": "니 소식"}
+{"title": "우리 어떻게 할까요", "rank": "16", "singer": "첸 (CHEN)", "album": "사랑하는 그대에게 - The 2nd Mini Album"}
+{"title": "기억해줘요 내 모든 날과 그때를", "rank": "17", "singer": "거미", "album": "호텔 델루나 OST Part.7"}
+{"title": "포장마차", "rank": "18", "singer": "황인욱", "album": "포장마차"}
+{"title": "만추 (Feat. Crush)", "rank": "19", "singer": "헤이즈 (Heize)", "album": "만추"}
+{"title": "가끔 이러다", "rank": "20", "singer": "펀치 (Punch)", "album": "가끔 이러다"}
+{"title": "헤어지자 (Prod. 정키)", "rank": "21", "singer": "휘인 (Whee In)", "album": "soar"}
+{"title": "2002", "rank": "22", "singer": "Anne-Marie", "album": "Speak Your Mind (Deluxe)"}
+{"title": "Señorita", "rank": "23", "singer": "Shawn Mendes", "album": "Señorita"}
+{"title": "천둥벌거숭이 (Feat. Jvcki Wai, 염따)", "rank": "24", "singer": "지코 (ZICO)", "album": "THINKING Part.1"}
+{"title": "사랑에 연습이 있었다면 (Prod. 2soo)", "rank": "25", "singer": "임재현", "album": "사랑에 연습이 있었다면"}
+{"title": "그대라는 시", "rank": "26", "singer": "태연 (TAEYEON)", "album": "호텔 델루나 OST Part.3"}
+{"title": "술이 문제야", "rank": "27", "singer": "장혜진", "album": "술이 문제야"}
+{"title": "사랑이 식었다고 말해도 돼", "rank": "28", "singer": "먼데이 키즈 (Monday Kiz)", "album": "사랑이 식었다고 말해도 돼"}
+{"title": "작은 것들을 위한 시 (Boy With Luv) feat. Halsey", "rank": "29", "singer": "방탄소년단", "album": "MAP OF THE SOUL : PERSONA"}
+{"title": "모든 날, 모든 순간 (Every day, Every Moment)", "rank": "30", "singer": "폴킴", "album": "`키스 먼저 할까요?` OST Part.3"}
+{"title": "bad guy", "rank": "31", "singer": "Billie Eilish", "album": "WHEN WE ALL FALL ASLEEP, WHERE DO WE GO?"}
+{"title": "호랑이소굴 (Feat. Jvcki Wai)", "rank": "32", "singer": "기리보이", "album": "호랑이소굴"}
+{"title": "사람", "rank": "33", "singer": "지코 (ZICO)", "album": "THINKING Part.1"}
+{"title": "이별행동", "rank": "34", "singer": "이우", "album": "이별행동"}
+{"title": "다시 만날까 봐", "rank": "35", "singer": "V.O.S", "album": "다시 만날까 봐"}
+{"title": "BAND", "rank": "36", "singer": "창모 (CHANGMO)", "album": "BAND"}
+{"title": "헤어져줘서 고마워", "rank": "37", "singer": "벤", "album": "헤어져줘서 고마워"}
+{"title": "복숭아", "rank": "38", "singer": "아이유", "album": "스무 살의 봄"}
+{"title": "나의 어깨에 기대어요", "rank": "39", "singer": "10cm", "album": "호텔 델루나 OST Part.2"}
+{"title": "Done For Me", "rank": "40", "singer": "펀치 (Punch)", "album": "호텔 델루나 OST Part.12"}
+{"title": "내 목소리 들리니", "rank": "41", "singer": "벤", "album": "호텔 델루나 OST Part.9"}
+{"title": "꽃처럼 예쁜 그대", "rank": "42", "singer": "임한별", "album": "동백꽃 필 무렵 (KBS2 수목드라마) OST - Part.4"}
+{"title": "달", "rank": "43", "singer": "AKMU (악동뮤지션)", "album": "항해"}
+{"title": "벗", "rank": "44", "singer": "닐로(Nilo)", "album": "벗"}
+{"title": "Speechless (Full)", "rank": "45", "singer": "Naomi Scott", "album": "Aladdin (Original Motion Picture Soundtrack)"}
+{"title": "사월이 지나면 우리 헤어져요 (Beautiful goodbye)", "rank": "46", "singer": "첸 (CHEN)", "album": "사월, 그리고 꽃 - The 1st Mini Album"}
+{"title": "널 안지 않을 수 있어야지", "rank": "47", "singer": "첸 (CHEN)", "album": "사랑하는 그대에게 - The 2nd Mini Album"}
+{"title": "너를 만나", "rank": "48", "singer": "폴킴", "album": "너를 만나"}
+{"title": "사계 (Four Seasons)", "rank": "49", "singer": "태연 (TAEYEON)", "album": "사계 (Four Seasons)"}
+{"title": "내 맘을 볼 수 있나요", "rank": "50", "singer": "헤이즈 (Heize)", "album": "호텔 델루나 OST Part.5"}
+{"title": "날라리 (LALALAY)", "rank": "51", "singer": "선미", "album": "날라리 (LALALAY)"}
+{"title": "우리 시작해도 괜찮을까요", "rank": "52", "singer": "정준일", "album": "우리 시작해도 괜찮을까요"}
+{"title": "헤어질 자신 있니 (Narr. SBS 박선영 아나운서)", "rank": "53", "singer": "앤씨아", "album": "헤어질 자신 있니"}
+{"title": "솔직하게 말해서 나", "rank": "54", "singer": "김나영", "album": "솔직하게 말해서 나"}
+{"title": "너에게 못했던 내 마지막 말은", "rank": "55", "singer": "다비치", "album": "너에게 못했던 내 마지막 말은"}
+{"title": "그때가 좋았어", "rank": "56", "singer": "케이시 (Kassy)", "album": "그때가 좋았어"}
+{"title": "Paris In The Rain", "rank": "57", "singer": "Lauv", "album": "I met you when I was 18. (the playlist)"}
+{"title": "노래방에서", "rank": "58", "singer": "장범준", "album": "장범준 3집"}
+{"title": "내 안부", "rank": "59", "singer": "이우", "album": "내 안부"}
+{"title": "나빠(NAPPA)", "rank": "60", "singer": "Crush", "album": "나빠(NAPPA)"}
+{"title": "ICY", "rank": "61", "singer": "ITZY (있지)", "album": "IT`z ICY"}
+{"title": "전화를 할까봐", "rank": "62", "singer": "오반", "album": "전화를 할까봐"}
+{"title": "일기", "rank": "63", "singer": "헤이즈 (Heize)", "album": "만추"}
+{"title": "FANCY", "rank": "64", "singer": "TWICE (트와이스)", "album": "FANCY YOU"}
+{"title": "FREEDOM", "rank": "65", "singer": "AKMU (악동뮤지션)", "album": "항해"}
+{"title": "FLASH", "rank": "66", "singer": "X1 (엑스원)", "album": "비상 : QUANTUM LEAP"}
+{"title": "인사", "rank": "67", "singer": "멜로망스", "album": "인사"}
+{"title": "가을속에서", "rank": "68", "singer": "화사(Hwa Sa)", "album": "화실(化-室:atelier)"}
+{"title": "주저하는 연인들을 위해", "rank": "69", "singer": "잔나비", "album": "전설"}
+{"title": "꿈속에 너 (Feat. 전상근)", "rank": "70", "singer": "에이치코드 (H:CODE)", "album": "두 번째 이야기"}
+{"title": "Snapping", "rank": "71", "singer": "청하", "album": "Flourishing"}
+{"title": "봄날", "rank": "72", "singer": "방탄소년단", "album": "YOU NEVER WALK ALONE"}
+{"title": "그대에게", "rank": "73", "singer": "첸 (CHEN)", "album": "사랑하는 그대에게 - The 2nd Mini Album"}
+{"title": "괜찮아도 괜찮아 (That`s okay)", "rank": "74", "singer": "디오 (D.O.)", "album": "괜찮아도 괜찮아 (That`s okay) - SM STATION"}
+{"title": "가라사대", "rank": "75", "singer": "BewhY (비와이)", "album": "The Movie Star"}
+{"title": "Believer", "rank": "76", "singer": "Imagine Dragons", "album": "Evolve"}
+{"title": "물 만난 물고기", "rank": "77", "singer": "AKMU (악동뮤지션)", "album": "항해"}
+{"title": "고운 그대는 시들지 않으리", "rank": "78", "singer": "첸 (CHEN)", "album": "사랑하는 그대에게 - The 2nd Mini Album"}
+{"title": "담아 (Feat. 염따, pH-1) (Prod. BOYCOLD)", "rank": "79", "singer": "스윙스", "album": "쇼미더머니 8 Episode 1"}
+{"title": "Another Day", "rank": "80", "singer": "먼데이 키즈 (Monday Kiz)", "album": "호텔 델루나 OST Part.1"}
+{"title": "그댄 모르죠", "rank": "81", "singer": "첸 (CHEN)", "album": "사랑하는 그대에게 - The 2nd Mini Album"}
+{"title": "IDOL", "rank": "82", "singer": "방탄소년단", "album": "LOVE YOURSELF 結 `Answer`"}
+{"title": "사랑 둘", "rank": "83", "singer": "김필", "album": "사랑 둘"}
+{"title": "밤 끝없는 밤", "rank": "84", "singer": "AKMU (악동뮤지션)", "album": "항해"}
+{"title": "아퍼 (Feat. Kid Milli, Lil tachi, 김승민, NO:EL, C JAMM)", "rank": "85", "singer": "기리보이", "album": "100년제전문대학"}
+{"title": "굿밤 (GOOD BAM)", "rank": "86", "singer": "엔플라잉 (N.Flying)", "album": "야호(夜好)"}
+{"title": "나만 아픈 일", "rank": "87", "singer": "이해리 (다비치)", "album": "나만 아픈 일"}
+{"title": "비", "rank": "88", "singer": "폴킴", "album": "비"}
+{"title": "뱃노래", "rank": "89", "singer": "AKMU (악동뮤지션)", "album": "항해"}
+{"title": "DAUM (Feat. Colde)", "rank": "90", "singer": "헤이즈 (Heize)", "album": "만추"}
+{"title": "We don`t talk together (Feat. 기리보이) (Prod. SUGA)", "rank": "91", "singer": "헤이즈 (Heize)", "album": "We don`t talk together"}
+{"title": "달라달라", "rank": "92", "singer": "ITZY (있지)", "album": "IT`z Different"}
+{"title": "잘 자요", "rank": "93", "singer": "첸 (CHEN)", "album": "사랑하는 그대에게 - The 2nd Mini Album"}
+{"title": "Goodbye", "rank": "94", "singer": "박효신", "album": "Goodbye"}
+{"title": "음파음파 (Umpah Umpah)", "rank": "95", "singer": "Red Velvet (레드벨벳)", "album": "‘The ReVe Festival’ Day 2"}
+{"title": "사계 (하루살이)", "rank": "96", "singer": "엠씨더맥스", "album": "Circular"}
+{"title": "Love Shot", "rank": "97", "singer": "EXO", "album": "LOVE SHOT - The 5th Album Repackage"}
+{"title": "XX", "rank": "98", "singer": "볼빨간사춘기", "album": "Two Five"}
+{"title": "Way Back Home", "rank": "99", "singer": "숀 (SHAUN)", "album": "Take"}
+{"title": "우리 만난다면", "rank": "100", "singer": "스무살", "album": "낮과 밤"}
